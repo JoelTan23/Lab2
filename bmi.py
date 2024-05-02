b5fe697f2649fe40c2faed600cbb6d34851e90ca
Joel <joeltan16.23@ichat.sp.edu.sg>
2024-05-02 10:57:31 +0800

    For lab3

--- bmi.py
+++ bmi.py
@@ -7,12 +7,12 @@ def calculate_bmi (height,weight):
     print ("bmi = " + str(bmi))
 
     if bmi < 18.5:
-        print ("Under Weight", -1)
+        print ("Under Weight", -1) #-1 for lab3
         
     elif bmi > 25.0:
-        print ("Over Weight", 0)
+        print ("Over Weight", 0)# 0 for lab3
     else:
-        print ("Normal Weight", 1)
+        print ("Normal Weight", 1) #1 for lab3
     
 
 calculate_bmi(weight=57, height=1.73)
